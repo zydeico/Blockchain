0d0aa9572abe2a82e5f2cb6e62752501e9d11878
Daniel Vázquez <daniel.prolite@gmail.com>
2021-03-24 21:14:14 -0600

    After a day of coding, I need rest. Continue tomorrow

--- BlockChain.py
+++ BlockChain.py
@@ -19,6 +19,12 @@ class BlockChain:
         self.new_block(previous_hash="1", proof=100)
 
     def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: Address of node
+        'http://192.168.0.5:5000'
+        """
+
         parsed_url = urlparse(address)
         if parsed_url.netloc:
             self.nodes.add(parsed_url.netloc)
@@ -26,3 +32,30 @@ class BlockChain:
             self.nodes.add(parsed_url.path)
         else:
             raise ValueError('Invalid')
+
+    # Method for valid chain
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: A blockchain
+        :return: True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n____\n")
+
+            # Check that the hash of the block is correct
+            last_block_hash = self.hash(last_block)
+            if block['previous_hash'] != last_block_hash:
+                return False
+
+            last_block = block
+            current_index =+ 1
+
+        return True
